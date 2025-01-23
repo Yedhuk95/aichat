72b47572f96ef29688c938b38933137933857847
yedhu <yedhuk95@gmail.com>
2025-01-23 15:00:57 +0530

    /apipath

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-const API_URL = "https://aichat-gemini.vercel.app/"; // Backend server URL
+//const API_URL = "https://aichat-gemini.vercel.app/"; // Backend server URL
 
 // Function to handle file upload
 async function uploadFile() {
@@ -14,7 +14,7 @@ async function uploadFile() {
   formData.append("file", file);
 
   try {
-    const response = await fetch(`${API_URL}/api/upload`, {
+    const response = await fetch(`/api/upload`, {
       method: "POST",
       body: formData,
     });
@@ -66,7 +66,7 @@ async function askAI() {
   }
 
   try {
-    const response = await fetch(`${API_URL}/api/ask`, {
+    const response = await fetch(`/api/ask`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ content, question }),
